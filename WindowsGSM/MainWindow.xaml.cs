f3f9cc7834c38236bb13e07e71ed24be38b8e2a3
RocketMyrr <221737+Myrddinx@users.noreply.github.com>
2021-02-26 20:29:14 -0500

    add umod support

--- WindowsGSM/MainWindow.xaml.cs
+++ WindowsGSM/MainWindow.xaml.cs
@@ -3443,6 +3443,8 @@ namespace WindowsGSM
             var server = (ServerTable)ServerGrid.SelectedItem;
             if (server == null) { return; }
 
+            if (GetServerMetadata(server.ID).ServerStatus != ServerStatus.Stopped) { return; }
+
             string messageTitle = "Tools - Install OxideMod";
 
             bool? existed = InstallAddons.IsOxideModExists(server);
@@ -3480,6 +3482,63 @@ namespace WindowsGSM
                 await this.ShowMessageAsync(messageTitle, $"{message} (ID: {server.ID})");
             }
         }
+
+        private async void Tools_Install_UMOD_Click(object sender, RoutedEventArgs e)
+        {
+            var server = (ServerTable)ServerGrid.SelectedItem;
+            if (server == null) { return; }
+
+            if (GetServerMetadata(server.ID).ServerStatus != ServerStatus.Stopped) { return; }
+
+            string messageTitle = "Tools - Install uMOD";
+
+            var result = await this.ShowMessageAsync(messageTitle, $"Are you sure you want to install uMOD? (ID: {server.ID})", MessageDialogStyle.AffirmativeAndNegative);
+            if (result == MessageDialogResult.Affirmative)
+            {
+                _serverMetadata[int.Parse(server.ID)].ServerStatus = ServerStatus.Updating;
+                Log(server.ID, "Action: Install/Update uMOD");
+                SetServerStatus(server, "Updating");
+
+                ProgressDialogController controller = await this.ShowProgressAsync("Installing...", "Please wait...");
+                controller.SetIndeterminate();
+                Process umod = new Process();
+                umod.StartInfo.FileName = "PowerShell.exe";
+                umod.StartInfo.Arguments = "&powershell -NoProfile -ExecutionPolicy unrestricted -Command \"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://umod.io/umod-develop.ps1')))\"";
+                umod.Start();
+                umod.WaitForExit();
+                await controller.CloseAsync();
+
+
+                string workingDir = ServerPath.GetServersServerFiles(server.ID);
+                if (server.Game.Contains("Valheim"))
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = "PowerShell.exe";
+                    proc.StartInfo.Arguments = "umod install valheim -P; umod new launcher -P";
+                    proc.StartInfo.WorkingDirectory = workingDir;
+                    proc.Start();
+                    proc.WaitForExit();
+                    await this.ShowMessageAsync(messageTitle, $"Installed Succesfully (ID: {server.ID})");
+                }
+
+                if (server.Game.Contains("Rust"))
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = "PowerShell.exe";
+                    proc.StartInfo.Arguments = "umod install rust -P; umod new launcher -P";
+                    proc.StartInfo.WorkingDirectory = workingDir;
+                    proc.Start();
+                    proc.WaitForExit();
+                    await this.ShowMessageAsync(messageTitle, $"Installed Succesfully (ID: {server.ID})");
+                }
+
+                _serverMetadata[int.Parse(server.ID)].ServerStatus = ServerStatus.Stopped;
+                Log(server.ID, "Action: Install/Update uMOD Complete");
+                SetServerStatus(server, "Stopped");
+
+            }
+        }
+
 
         private async void Tools_UpdateAIO_Click(object sender, RoutedEventArgs e)
         {
